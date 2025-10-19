5e45e7f6c8f34e61c5fbbf1ab8ce2e1af401ef0e
Gabriel Janicas <gabrieljanicas@ua.pt>
2025-10-19 22:36:05 +0100

    pequenas alterações

---
+++ Lab_1/bit_stream/src/build/CMakeFiles/bin2text.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/janicas/Documents/Universidade/5ºAno/IC/bit_stream/bin/bin2text"
+  "/home/janicas/Documents/Universidade/5ºAno/IC/bit_stream/bin/bin2text.pdb"
+  "CMakeFiles/bin2text.dir/bin2text.cpp.o"
+  "CMakeFiles/bin2text.dir/bin2text.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bin2text.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lab_1/bit_stream/src/build/CMakeFiles/collect_data.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/janicas/Documents/Universidade/5ºAno/IC/bit_stream/bin/collect_data"
+  "/home/janicas/Documents/Universidade/5ºAno/IC/bit_stream/bin/collect_data.pdb"
+  "CMakeFiles/collect_data.dir/collect_data.cpp.o"
+  "CMakeFiles/collect_data.dir/collect_data.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/collect_data.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
